a676ad682569f89e1eafc9ce4389a2fa61838ace
rahul jain <rahuljain14314@gmail.com>
2023-09-27 14:36:10 +0530

    New FrontEnd

---
+++ frontend/src/Components/Player/PlayerForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class PlayerForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {};
+  render() {
+    return <div>PlayerForm</div>;
+  }
+}
+
+export default PlayerForm;
